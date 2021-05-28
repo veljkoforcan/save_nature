1aa3bb7d035c21d0fc117bc16f263ce0f1aefd2b
veljkoforcan <veljko_forcan@hotmail.com>
2021-05-28 20:27:22 +0200

    Initial commit

---
+++ wp-content/themes/xcel/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Xcel
+ */
+echo 'Copyright &copy; Sačuvajmo prirodu';
+?>
+</div><!-- #content -->
+
+<?php if ( get_theme_mod( 'xcel-setting-footer-layout', false ) == 'xcel-setting-footer-layout-none' ) : ?>
+
+    <?php get_template_part( '/templates/footers/footer-none' ); ?>
+    
+<?php else : ?>
+	
+	<?php get_template_part( '/templates/footers/footer-standard' ); ?>
+    
+<?php endif; ?>
+
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
